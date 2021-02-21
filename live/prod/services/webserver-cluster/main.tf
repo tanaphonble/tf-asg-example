d6fd8cbce35c27cf9a0fa9e3a0a01aac63bb6d04
Ble <a.tanaphon@gmail.com>
2021-02-21 11:32:27 +0700

    use modules v0.1.3

--- live/prod/services/webserver-cluster/main.tf
+++ live/prod/services/webserver-cluster/main.tf
@@ -9,7 +9,7 @@ terraform {
 }
 
 module "webserver_cluster" {
-  source = "github.com/tanaphonble/tf-modules//services/webserver-cluster?ref=v0.0.2"
+  source = "github.com/tanaphonble/tf-modules//services/webserver-cluster?ref=v0.1.3"
 
   cluster_name           = "webservers-stage"
   db_remote_state_bucket = "terraform-state-rdnudxq0"
@@ -18,26 +18,6 @@ module "webserver_cluster" {
   instance_type = "t2.micro"
   min_size      = 2
   max_size      = 10
+
+  enable_autoscaling = true
 }
-
-resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
-  scheduled_action_name = "scale-out-during-business-hours"
-  min_size              = 2
-  max_size              = 10
-  desired_capacity      = 10
-  recurrence            = "0 9 * * *"
-
-  autoscaling_group_name = module.webserver_cluster.asg_name
-}
-
-resource "aws_autoscaling_schedule" "scale_in_at_night" {
-  scheduled_action_name = "scale-in-at-night"
-  min_size              = 2
-  max_size              = 10
-  desired_capacity      = 2
-  recurrence            = "0 17 * * *"
-
-  autoscaling_group_name = module.webserver_cluster.asg_name
-}
-
-
