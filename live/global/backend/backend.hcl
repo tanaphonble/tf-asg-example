b26480bc9de9e92e603e4dbb0af32df58c6489dd
Ble <a.tanaphon@gmail.com>
2021-02-28 13:31:52 +0700

    add outout for test

---
+++ live/global/backend/backend.hcl
@@ -0,0 +1,5 @@
+key            = "global/backend/terraform.tfstate"
+bucket         = "terraform-state-rdnudxq0"
+region         = "ap-southeast-1"
+dynamodb_table = "terraform-state-locks-rdnudxq0"
+encrypt        = true
--- live/global/backend/outputs.tf
+++ live/global/backend/outputs.tf
@@ -7,3 +7,14 @@ output "dynamodb_table_name" {
   value       = aws_dynamodb_table.terraform_locks.name
   description = "The name of the DynamoDB table"
 }
+
+output "s3_bucket_test_arn" {
+  value       = aws_s3_bucket.terraform_state_test.arn
+  description = "The ARNof the S3 bucket for testing"
+}
+
+output "dynamodb_table_test_name" {
+  value       = aws_dynamodb_table.terraform_locks_test.name
+  description = "The name of the DynamoDB table for testing"
+}
+
