8804eb3975f01db24de59e030c574c4cb703faa6
Ble <a.tanaphon@gmail.com>
2021-02-28 13:25:04 +0700

    create backend for env test

--- live/global/backend/main.tf
+++ live/global/backend/main.tf
@@ -39,3 +39,35 @@ resource "aws_dynamodb_table" "terraform_locks" {
     type = "S"
   }
 }
+
+resource "aws_s3_bucket" "terraform_state_test" {
+  bucket = "terraform-state-for-testing"
+
+  # Prevent accidential deletion of this S3 bucket
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks_test" {
+  name         = "terraform-state-locks-for-testing"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
